622d1a80e0fe90ef4c5895f3925787b5aaf3d788
Chisa Domatsu <swing.hokke@gmail.com>
2019-12-06 18:00:56 +0900

    日柄のいい日が混む条件なので、WeatherHacks APIを使って今日明日明後日の天気予報を取得して表示までしました。 まだ、日柄のいい日判定はしていません。

--- KuyakusyoChecker/Form1.Designer.cs
+++ KuyakusyoChecker/Form1.Designer.cs
@@ -28,13 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Font = new System.Drawing.Font("MS UI Gothic", 20F);
+            this.StartButton.Location = new System.Drawing.Point(307, 294);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(150, 50);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Check";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarFont = new System.Drawing.Font("MS UI Gothic", 10F);
+            this.dateTimePicker1.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.dateTimePicker1.Location = new System.Drawing.Point(223, 209);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(343, 32);
+            this.dateTimePicker1.TabIndex = 1;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.StartButton);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
 
--- KuyakusyoChecker/Form1.cs
+++ KuyakusyoChecker/Form1.cs
@@ -6,15 +6,101 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 using System.Windows.Forms;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
 
 namespace KuyakusyoChecker
 {
     public partial class Form1 : Form
     {
+        const String citycode = "230010";
+        String url = "http://weather.livedoor.com/forecast/webservice/json/v1?city=" + citycode;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            string[] RokuyoLabel = { "大安", "赤口", "先勝", "友引", "先負", "仏滅" };
+            var TheDay = dateTimePicker1.Value;
+            string DayOfW = TheDay.ToString("dddd");
+            int month = TheDay.Month;
+            //日を取得する。「30」となる。
+            int day = TheDay.Day;
+            if ((DayOfW == "土曜日") || (DayOfW == "日曜日"))
+            {
+                MessageBox.Show(DayOfW + "なのでお休みです");
+                return;
+            }
+            var Today = DateTime.Today;
+
+            string json = new HttpClient().GetStringAsync(url).Result;
+            JObject jobj = JObject.Parse(json);
+            double interval = (TheDay - Today).TotalDays;
+            if((interval >= 0) && (interval <= 2))
+            {
+                string todayweather = (string)((jobj["forecasts"][(int)interval]["telop"] as JValue).Value);//今日明日明後日の天気の取得
+                MessageBox.Show(todayweather);
+            }
+            
+
+            //旧暦を調べる
+            var OldCalender = new JapaneseLunisolarCalendar();
+            int OldM = OldCalender.GetMonth(TheDay);
+            int OldD = OldCalender.GetDayOfMonth(TheDay);
+            
+            //閏月を調べる(閏月が無いときは0が返る)
+            DateTime UruF = OldCalender.AddDays(TheDay, 1 - OldCalender.GetDayOfYear(TheDay));
+            int UruM = OldCalender.GetLeapMonth(OldCalender.GetYear(UruF), OldCalender.GetEra(UruF));
+
+            //旧暦の月の訂正
+            if((UruM > 0)&&(OldM >= UruM))
+            {
+                OldM--;
+            }
+
+            string Rokuyo = RokuyoLabel[(OldM + OldD) % 6];
+
+            //多い条件を調べる
+            if(Rokuyo == "大安") //婚姻届がたくさん来る
+            {
+                count++;
+            }
+            if(DayOfW == "月曜日") //祝日の翌日
+            {
+                count++;
+            }
+            if(month == day) //ぞろ目の日
+            {
+                count++;
+            }
+            if((month == 12)&&(day == 24))
+            {
+                count++;
+            }
+            switch (count)
+            {
+                case 0:
+                    MessageBox.Show("あまり混んでいないと思う");
+                    break;
+                case 1:
+                case 2:
+                    MessageBox.Show("かなり混んでいると思う");
+                    break;
+                default:
+                    MessageBox.Show("むっちゃんこ混んでいると思う");
+                    break;
+            }
+            
         }
     }
 }
